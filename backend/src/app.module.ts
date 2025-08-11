3b83eea92eabd7bdccbd6c2ccd70a25130f20211
tomtomjskim <124156309+tomtomjskim@users.noreply.github.com>
2025-08-11 17:33:14 +0900

    chore: use env variables and optimize build

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -14,11 +14,7 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT || '5432', 10),
-      username: process.env.DB_USER || 'postgres',
-      password: process.env.DB_PASS || 'postgres',
-      database: process.env.DB_NAME || 'lunch',
+      url: process.env.DATABASE_URL,
       entities: [User, Food, Category, FoodCategory, Preference],
       synchronize: true,
     }),
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { JwtStrategy } from './jwt.strategy';
     TypeOrmModule.forFeature([User]),
     PassportModule,
     JwtModule.register({
-      secret: process.env.JWT_SECRET || 'secret',
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '1h' },
     }),
   ],
