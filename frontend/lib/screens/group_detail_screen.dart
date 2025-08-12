ceac1ceee9f7ead883bf04f031e8c56378e4fb15
tomtomjskim <124156309+tomtomjskim@users.noreply.github.com>
2025-08-12 10:46:34 +0900

    Add poll screens and integrate with group details

--- frontend/lib/screens/group_detail_screen.dart
+++ frontend/lib/screens/group_detail_screen.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import '../widgets/banner_placeholder.dart';
 import '../services/apis/groups_api.dart';
+import '../services/apis/poll_api.dart';
 
 class GroupDetailScreen extends StatefulWidget {
   final String groupId;
@@ -13,6 +15,13 @@ class GroupDetailScreen extends StatefulWidget {
 class _GroupDetailScreenState extends State<GroupDetailScreen> {
   final GroupsApi api = GroupsApi();
   late Future<Map<String, dynamic>> _group;
+  int? _pollId;
+
+  void _onPollCreated(Map<String, dynamic> poll) {
+    setState(() {
+      _pollId = poll['id'] as int?;
+    });
+  }
 
   @override
   void initState() {
@@ -47,6 +56,16 @@ class _GroupDetailScreenState extends State<GroupDetailScreen> {
               Text('Members', style: Theme.of(context).textTheme.titleMedium),
               ...members.map((m) => ListTile(title: Text(m['name'] ?? 'Member'))),
               const SizedBox(height: 24),
+              PollCreationForm(
+                groupId: widget.groupId,
+                onCreated: _onPollCreated,
+              ),
+              const SizedBox(height: 24),
+              if (_pollId != null)
+                PollStatusWidget(
+                  pollId: _pollId!,
+                ),
+              const SizedBox(height: 24),
               if (isOwner)
                 ElevatedButton(
                   onPressed: () {},
@@ -62,6 +81,118 @@ class _GroupDetailScreenState extends State<GroupDetailScreen> {
         },
       ),
       bottomNavigationBar: const BannerPlaceholder(),
+    );
+  }
+}
+
+class PollCreationForm extends StatefulWidget {
+  final String groupId;
+  final void Function(Map<String, dynamic>) onCreated;
+
+  const PollCreationForm({
+    super.key,
+    required this.groupId,
+    required this.onCreated,
+  });
+
+  @override
+  State<PollCreationForm> createState() => _PollCreationFormState();
+}
+
+class _PollCreationFormState extends State<PollCreationForm> {
+  final _questionController = TextEditingController();
+  final List<TextEditingController> _optionControllers = [
+    TextEditingController(),
+    TextEditingController(),
+  ];
+
+  void _addOptionField() {
+    setState(() {
+      _optionControllers.add(TextEditingController());
+    });
+  }
+
+  Future<void> _submit() async {
+    final options =
+        _optionControllers.map((c) => c.text).where((t) => t.isNotEmpty).toList();
+    final poll = await PollApi(null)
+        .createPoll(_questionController.text, options, widget.groupId);
+    widget.onCreated(poll);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('Start a Poll'),
+        TextField(
+          controller: _questionController,
+          decoration: const InputDecoration(labelText: 'Question'),
+        ),
+        ..._optionControllers
+            .map((c) => TextField(
+                  controller: c,
+                  decoration: const InputDecoration(labelText: 'Option'),
+                ))
+            .toList(),
+        TextButton(onPressed: _addOptionField, child: const Text('Add Option')),
+        ElevatedButton(onPressed: _submit, child: const Text('Create Poll')),
+      ],
+    );
+  }
+}
+
+class PollStatusWidget extends StatefulWidget {
+  final int pollId;
+
+  const PollStatusWidget({super.key, required this.pollId});
+
+  @override
+  State<PollStatusWidget> createState() => _PollStatusWidgetState();
+}
+
+class _PollStatusWidgetState extends State<PollStatusWidget> {
+  Map<String, dynamic>? _poll;
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetch();
+    _timer = Timer.periodic(const Duration(seconds: 5), (_) => _fetch());
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  Future<void> _fetch() async {
+    final poll = await PollApi(null).getResults(widget.pollId);
+    setState(() {
+      _poll = poll;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_poll == null) {
+      return const Center(child: CircularProgressIndicator());
+    }
+    final options = _poll!['options'] as List<dynamic>? ?? [];
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(_poll!['question'] ?? ''),
+        ...options.map((o) {
+          final votes = (o['votes'] as List<dynamic>? ?? []).length;
+          return ListTile(
+            title: Text('${o['text']} ($votes)'),
+          );
+        }),
+      ],
     );
   }
 }
